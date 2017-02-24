77943ac0ce6116367c2350614574531b30ad1c58
Matus Nemec <xnemec1@fi.muni.cz>
2017-02-24 10:17:49 +0100

    Removed deprecated dataset reconstruction.

--- src/cz/crcs/sekan/rsakeysanalysis/classification/table/ClassificationContainer.java
+++ src/cz/crcs/sekan/rsakeysanalysis/classification/table/ClassificationContainer.java
@@ -14,41 +14,17 @@ public class ClassificationContainer {
     private long numOfRows = 0;
     private long numOfKeys = 0;
     private ClassificationRow row;
-    private List<ClassificationKey> keys; // necessary, if we want to reconstruct the original dataset
 
     public ClassificationContainer(long numOfDuplicityKeys, ClassificationRow row) {
         numOfRows = 1;
         numOfKeys = numOfDuplicityKeys;
         this.row = row;
-        this.keys = new LinkedList<>();
-    }
-
-    public ClassificationContainer(long numOfDuplicityKeys, ClassificationRow row, ClassificationKey key) {
-        numOfRows = 1;
-        numOfKeys = numOfDuplicityKeys;
-        this.row = row;
-        this.keys = new LinkedList<>();
-        keys.add(key);
     }
 
     public void add(long numOfDuplicityKeys, ClassificationRow row) {
         numOfRows++;
         numOfKeys += numOfDuplicityKeys;
         this.row = this.row.computeWithSameSource(row);
-    }
-
-    public void add(long numOfDuplicityKeys, ClassificationRow row, ClassificationKey key) {
-        numOfRows++;
-        numOfKeys += numOfDuplicityKeys;
-        this.row = this.row.computeWithSameSource(row);
-        keys.add(key);
-    }
-
-    public void add(long numOfDuplicityKeys, ClassificationRow row, List<ClassificationKey> keys) {
-        numOfRows++;
-        numOfKeys += numOfDuplicityKeys;
-        this.row = this.row.computeWithSameSource(row);
-        this.keys.addAll(keys);
     }
 
     public long getNumOfRows() {
@@ -61,10 +37,6 @@ public class ClassificationContainer {
 
     public ClassificationRow getRow() {
         return row;
-    }
-
-    public List<ClassificationKey> getKeys() {
-        return keys;
     }
 
     public JSONObject toJSON() {
