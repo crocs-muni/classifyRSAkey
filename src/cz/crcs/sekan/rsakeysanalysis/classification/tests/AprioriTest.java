125b15e8070a144148c56101db8770f99312a405
Matus Nemec <xnemec1@fi.muni.cz>
2017-03-02 15:15:55 +0100

    Added option to simulate unknown source for a priori test

--- src/cz/crcs/sekan/rsakeysanalysis/classification/tests/AprioriTest.java
+++ src/cz/crcs/sekan/rsakeysanalysis/classification/tests/AprioriTest.java
@@ -21,6 +21,9 @@ import java.util.function.UnaryOperator;
  */
 public class AprioriTest {
 
+    private static final String INTERCEPT_GROUP_NAME = "Intercept";
+    private static final double MAX_INTERCEPT_PROBABILITY = 0.05;
+
     public static void testEstimatePrecision(ClassificationConfiguration configuration) throws NoSuchAlgorithmException {
 
         List<String> groupNames = new ArrayList<>(configuration.classificationTable.getGroupsNames());
@@ -33,25 +36,50 @@ public class AprioriTest {
         BigDecimal sumDistanceEstimated = BigDecimal.ZERO;
         BigDecimal sumDistanceUniform = BigDecimal.ZERO;
 
-        for (int i = 0; i < configuration.keyCount; i++) {
-            PriorProbability randomProbability = PriorProbability.randomize(random, groupNames);
-            Map<String, BigDecimal> simulatedStatistic =
-                    simulateStatistics(configuration.classificationTable.makeCopy(), randomProbability.makeCopy());
-            PriorProbabilityEstimator estimator = new NonNegativeLeastSquaresFitPriorProbabilityEstimator(configuration.classificationTable.makeCopy());
-            estimator.setMaskToFrequency(simulatedStatistic);
-            PriorProbability estimatedProbability = estimator.computePriorProbability();
-            BigDecimal distance = randomProbability.distance(estimatedProbability);
-            sumDistanceEstimated = sumDistanceEstimated.add(distance);
-            sumDistanceUniform = sumDistanceUniform.add(randomProbability.distance(uniform));
-        }
+        for (boolean addRandomSource : new boolean[]{false, true}) {
 
-        System.out.println("Average distance from random prior distribution:");
-        System.out.println(String.format("    to estimated: %s",
-                sumDistanceEstimated.divide(BigDecimal.valueOf(configuration.keyCount),
-                        PriorProbability.BIG_DECIMAL_SCALE, BigDecimal.ROUND_HALF_EVEN)));
-        System.out.println(String.format("    to uniform:   %s",
-                sumDistanceUniform.divide(BigDecimal.valueOf(configuration.keyCount),
-                        PriorProbability.BIG_DECIMAL_SCALE, BigDecimal.ROUND_HALF_EVEN)));
+            for (int i = 0; i < configuration.keyCount; i++) {
+                ClassificationTable tableForSimulation = configuration.classificationTable.makeCopy();
+
+                PriorProbability randomProbability;
+
+                if (addRandomSource) {
+                    tableForSimulation = addRandomGroupToTable(tableForSimulation, random);
+                    randomProbability = PriorProbability.randomize(random, new ArrayList<>(tableForSimulation.getGroupsNames()));
+                    randomProbability.put(INTERCEPT_GROUP_NAME, BigDecimal.valueOf(random.nextDouble() * MAX_INTERCEPT_PROBABILITY));
+                    randomProbability = randomProbability.normalized();
+                } else {
+                    randomProbability = PriorProbability.randomize(random, groupNames);
+                }
+
+                Map<String, BigDecimal> simulatedStatistic =
+                        simulateStatistics(tableForSimulation, randomProbability.makeCopy());
+                PriorProbabilityEstimator estimator = new NonNegativeLeastSquaresFitPriorProbabilityEstimator(configuration.classificationTable.makeCopy());
+                estimator.setMaskToFrequency(simulatedStatistic);
+                PriorProbability estimatedProbability = estimator.computePriorProbability();
+
+                if (addRandomSource) {
+                    randomProbability.remove(INTERCEPT_GROUP_NAME);
+                    randomProbability = randomProbability.normalized();
+                }
+
+                BigDecimal distance = randomProbability.distance(estimatedProbability);
+                sumDistanceEstimated = sumDistanceEstimated.add(distance);
+                sumDistanceUniform = sumDistanceUniform.add(randomProbability.distance(uniform));
+            }
+
+            if (addRandomSource) {
+                System.out.println("\nA completely random group is added to simulate unknown groups which can cover up to "
+                        + MAX_INTERCEPT_PROBABILITY * 100 + "% of the simulated dataset");
+            }
+            System.out.println("Average distance from random prior distribution:");
+            System.out.println(String.format("    to estimated: %s",
+                    sumDistanceEstimated.divide(BigDecimal.valueOf(configuration.keyCount),
+                            PriorProbability.BIG_DECIMAL_SCALE, BigDecimal.ROUND_HALF_EVEN)));
+            System.out.println(String.format("    to uniform:   %s",
+                    sumDistanceUniform.divide(BigDecimal.valueOf(configuration.keyCount),
+                            PriorProbability.BIG_DECIMAL_SCALE, BigDecimal.ROUND_HALF_EVEN)));
+        }
     }
 
     private static Map<String, BigDecimal> simulateStatistics(ClassificationTable table, PriorProbability probability) {
@@ -74,8 +102,6 @@ public class AprioriTest {
 
         return maskToFrequency;
     }
-
-    private static final String INTERCEPT_GROUP_NAME = "Intercept";
 
     private static ClassificationTable addRandomGroupToTable(ClassificationTable table, Random random) {
         List<String> masks = table.getMasks();
